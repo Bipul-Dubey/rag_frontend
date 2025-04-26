253cab353cabea8cfbd5ddfa434911e5501869fe
Bipul Dubey <bipul0873@gmail.com>
2025-04-26 17:59:30 +0530

    fix: auth redirect

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -4,7 +4,12 @@ import React from "react";
 const Page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-950 text-white">
-      <SignIn routing="hash" signInUrl="/rag" afterSignInUrl={"/rag"} />
+      <SignIn
+        routing="hash"
+        signInUrl="/rag"
+        afterSignInUrl={"/rag"}
+        signUpUrl="/sign-up"
+      />
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export const metadata: Metadata = {
   title: "KnowYourDocs | RAG AI",
@@ -13,7 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body
           suppressHydrationWarning
